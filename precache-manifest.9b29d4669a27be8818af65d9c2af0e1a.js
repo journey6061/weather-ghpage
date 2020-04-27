de8c6d4fe624b064779e8ccb0b3b38c853a1cf0f
alexz <journey6061@gmail.com>
2020-04-27 18:22:06 -0400

    Updates

---
+++ precache-manifest.9b29d4669a27be8818af65d9c2af0e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8360eb87f1c3e22132175526852f497",
+    "url": "/weather-ghpage/index.html"
+  },
+  {
+    "revision": "c4bb339d98a53a354234",
+    "url": "/weather-ghpage/static/css/main.f38d3891.chunk.css"
+  },
+  {
+    "revision": "43a2c2a3e7209085a5ab",
+    "url": "/weather-ghpage/static/js/2.e85e2256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-ghpage/static/js/2.e85e2256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bb339d98a53a354234",
+    "url": "/weather-ghpage/static/js/main.c467151f.chunk.js"
+  },
+  {
+    "revision": "961da3f5e52b54300edb",
+    "url": "/weather-ghpage/static/js/runtime-main.a92a02ad.js"
+  }
+]);
